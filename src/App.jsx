7107e4c88059b26da2b56d013080ba4a8e399a24
Afghan Eka Pangestu <afghanekapangestu@gmail.com>
2022-12-04 20:18:13 +0700

    Change btnPushToVal Function

--- src/App.jsx
+++ src/App.jsx
@@ -4,37 +4,48 @@ import './App.css'
 function App() {
   const [result,setResult] = useState("")
   const [calc, setCalc] = useState("")
-  // const operator = ['/','*','-','+','=']
+  const operator = ['/','*','-','+','.']
 
   function updateCalc(){
-    setCalc(result)
+    const newVal = result.toString()
+    setCalc(newVal)
   }
 
   function deleteHandler() {
     setCalc("")
     setResult(0)
   }
+
+  function deleteOneChar(){
+    setCalc(calc.slice(0,-1))
+  }
+
   function btnPushToVal(e) {
-    if(e.target.value == 0  && calc == ""){
-      return 
+    let targetValue = e.target.value
+    let targetValueStr = targetValue.toString()
+    if(targetValue == 0  && calc == "0"){
+      return
     }
-    setCalc(current => [...current,e.target.value] )
+
+    if(operator.includes(targetValueStr) && operator.includes(calc.slice(-1))){return}
+
+    setCalc(calc + targetValue)
+    
   }
 
   useEffect(()=>{
+    const arrCalc = [...calc]
+    console.log(arrCalc)
+    const arrCalcJoin = arrCalc.join('')
     try {
-      let detailResult = eval(calc.join(''))
-     
-      setResult(detailResult)
+      const newVal = eval(arrCalcJoin)
+      setResult(newVal)
     } catch (error) {
-      if(error && result=="" && calc==""){
-        setResult(0)
-      }else if(error && result != "" && calc!=0){
-        setResult(calc)
-      }
+      
     }
-    }
-  )
+
+  },[calc,result])
+  
 
 
   return (
@@ -42,39 +53,39 @@ function App() {
       <h1>Calculator</h1>
       <div className="input-wrapper">
         <p>
-          <span id='detail'>(
-            {result} 
-          )</span>
-          {calc || 0}
+          <span id='detail'>
+            ({result || '0'} )</span>
+          {calc || '0'}
         </p>
       </div>
       <div className='wrapper'>
         <div className="wrapper-inside">
           <button onClick={deleteHandler}>DEL</button>
+          <button onClick={deleteOneChar}>{"->"}</button>
         </div>
         <div className="wrapper-inside">
-          <button onClick={btnPushToVal} value="7">7</button>
-          <button onClick={btnPushToVal}  value="8">8</button>
-          <button onClick={btnPushToVal}  value="9">9</button>
-          <button onClick={btnPushToVal}  className="btnRight" value="/">/</button>
+          <button onClick={btnPushToVal} value={"7"}>7</button>
+          <button onClick={btnPushToVal}  value={"8"}>8</button>
+          <button onClick={btnPushToVal}  value={"9"}>9</button>
+          <button onClick={btnPushToVal}  className="btnRight" value={"/"}>/</button>
         </div>
         <div className="wrapper-inside">
-          <button onClick={btnPushToVal}  value="6">6</button>
-          <button onClick={btnPushToVal}  value="5">5</button>
-          <button onClick={btnPushToVal}  value="4">4</button>
-          <button onClick={btnPushToVal}  className="btnRight" value="*">*</button>
+          <button onClick={btnPushToVal}  value={"6"}>6</button>
+          <button onClick={btnPushToVal}  value={"5"}>5</button>
+          <button onClick={btnPushToVal}  value={"4"}>4</button>
+          <button onClick={btnPushToVal}  className="btnRight" value={"*"}>*</button>
         </div>
         <div className="wrapper-inside">
-          <button onClick={btnPushToVal}  value="3">3</button>
-          <button onClick={btnPushToVal}  value="2">2</button>
-          <button onClick={btnPushToVal}  value="1">1</button>
-          <button onClick={btnPushToVal}  className="btnRight" value="-">-</button>
+          <button onClick={btnPushToVal}  value={"3"}>3</button>
+          <button onClick={btnPushToVal}  value={"2"}>2</button>
+          <button onClick={btnPushToVal}  value={"1"}>1</button>
+          <button onClick={btnPushToVal}  className="btnRight" value={"-"}>-</button>
         </div>
         <div className="wrapper-inside">
-          <button onClick={btnPushToVal} value="0">0</button>
-          <button onClick={btnPushToVal} value=".">.</button>
+          <button onClick={btnPushToVal} value={"0"}>0</button>
+          <button onClick={btnPushToVal} value={"."}>.</button>
           <button onClick={updateCalc} id='btnSamaDengan'>=</button>
-          <button onClick={btnPushToVal} value="+" className="btnRight">+</button>
+          <button onClick={btnPushToVal} value={"+"} className="btnRight">+</button>
         </div>
       </div>
     </div>
